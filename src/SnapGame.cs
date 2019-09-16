390b66343a5a9a1d8983de45bcc70f530b7c549d
ifaaz-21 <ifaazmurshideen21@gmail.com>
2019-09-16 16:59:38 +1000

    add player hit with left and right shift, with score decrement for miss hits

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -23,26 +23,26 @@ namespace CardGames
 			//Fetch the next batch of UI interaction
 			SwinGame.ProcessEvents();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
-			{
-				myGame.Start();
-			}
-            if(myGame.IsStarted)
+            if (SwinGame.KeyTyped(KeyCode.vk_SPACE))
             {
-                if( SwinGame.KeyTyped(KeyCode.vk_LSHIFT) && SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+                myGame.Start();
+            }
+            if (myGame.IsStarted)
+            {
+                if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT) && SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
                 {
                     //TODO: add sound effects
                 }
-                else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT))
+                else if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT))
                 {
                     myGame.PlayerHit(0);
                 }
-                else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
+                else if (SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
                 {
                     myGame.PlayerHit(1);
                 }
             }
-		}
+        }
 
 		/// <summary>
 		/// Draws the game to the Window.
